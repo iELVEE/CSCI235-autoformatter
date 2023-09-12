f4a440e584006bc1e93b9cc9a75fbd3f910c7648
iELVEE <ommarashid@gmail.com>
2023-09-12 01:09:01 -0400

    single file formatting completely working as intended (see test files)

--- doxygen-formatter.cpp
+++ doxygen-formatter.cpp
@@ -13,9 +13,56 @@ Credits     : N/A
 #include <string>
 
 
-/** Take an input text file and format it to a doxygen style file
-* @pre  : text file to read
-* @post : specified file in this style of commented documentation is created
+/** Format a given line of the string to get rid of white spaces in comment blocks and leave the rest as is.
+* This function has 2 static variables to keep track of whether or not we are currently in a comment block or not and to know
+* whether or not the comment block has just ended. This is to know whether we should get rid of white spaces or leave them as
+* is. If we are in a comment block, we look for if the line is just white space and then remove it if it has no meaningful content.
+* If we are not in a comment block, we check for if the comment block has just ended, to get rid of the following empty line before
+* the function name. Lastly, we leave the line as is if neither conditions are true. The last bit of this function simply sets our
+* static flags true or false depending on if we find a "/*" or "*./" beginning/ending the comment block.
+* @param    : reference to a string, line of the input file to format
+* @post     : a modified string that is empty or contains the given line with an added new line for readability
+*/
+void formatLine(std::string& s)
+{
+    static bool in_comment_block = false;
+    static bool comment_block_just_ended = false;
+
+    if (in_comment_block)
+    {
+        if (s.find_first_not_of(' ') == std::string::npos)
+        {
+            s = "";
+        }
+        else
+        {
+            s += "\n";
+        }
+    }
+    else if (comment_block_just_ended)
+    {
+        s = "";
+        comment_block_just_ended = false;
+    }
+    else
+    {
+        s += "\n";
+    }
+
+    if (s.find("/**") != std::string::npos)
+    {
+        in_comment_block = true;
+    }
+    else if (s.find("*/") != std::string::npos)
+    {
+        in_comment_block = false;
+        comment_block_just_ended = true;
+    }
+}
+
+/** Take an input text file and format it to a doxygen style file.
+* @pre      : text file to read
+* @post     : specified file in this style of commented documentation is created
 */
 void routineOne()
 {
@@ -33,7 +80,8 @@ void routineOne()
         while (project_specs.good())
         {
             std::getline(project_specs, line);
-            formatted << line << std::endl;
+            formatLine(line);
+            formatted << line;
         }
         
     }
