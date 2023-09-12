624518a834b32dba0de7c4d96e1bb43a160f2035
iELVEE <ommarashid@gmail.com>
2023-09-12 14:02:19 -0400

    file filtering setting working for both routines

--- doxygen-formatter.cpp
+++ doxygen-formatter.cpp
@@ -15,6 +15,24 @@ Credits     : N/A
 #include <vector>
 #include <cctype>
 
+enum Doxs { AS_IS, JAVADOC, QT };
+std::vector<std::string> filter_types = { ".cpp", ".hpp", ".txt" };
+
+/***************************************************************************************************************************************
+ * SETTINGS
+ **************************************************************************************************************************************/
+
+bool filter_ = true;
+// Display only hpp and cpp files.
+
+Doxs doxygen_style_ = JAVADOC;
+// Use the specified doxygen comment style, AS_IS weill leave the comments as is with no modification.
+
+bool hpp_to_cpp_ = true;
+// If there are hpp files, copy the contents into a corresponding cpp file of the same name, even if the cpp file does not exist.
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 /** Format a given line of the string to get rid of white spaces in comment blocks and leave the rest as is.
 * This function has 2 static variables to keep track of whether or not we are currently in a comment block or not and to know
@@ -93,8 +111,44 @@ void overwriteFile(std::string input_file)
     ofile.close();
 }
 
+
+/** Check if the file is included among the filtered filetypes.
+* @param    : string of the file's name
+* @return   : bool of if the file type passes the filter
+*/
+bool checkExtension(std::string file_name)
+{
+    for (const auto& extension : filter_types)
+    {
+        if (file_name.find(extension) != std::string::npos)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+/** Add the file name to the vector of files through a reference and iterate i for printing the list
+* @param    : reference to a vector of strings, file names
+* @param    : file name to add to the vector
+* @param    : reference to i, an index of where the file name lies
+* @param    : boolean for if the list of the files should be printed out to cli
+* @post     : file name is added to vector, iterator is increased
+*/
+void addFile(std::vector<std::string>& files, std::string file_name, int& i, bool listMode)
+{
+    files.push_back(file_name);
+    if (listMode)
+    {
+        std::cout << "[" << i << "] " << file_name << std::endl;
+    }
+    i++;
+}
+
+
 /** List all regular files in a given directory.
 * @param    : string of directory
+* @param    : boolean for if the list of the files should be printed out to cli
 * @return   : vector of strings of all files in the directory
 */
 std::vector<std::string> selectionMenu(std::string selected_path, bool listMode = false)
@@ -105,12 +159,18 @@ std::vector<std::string> selectionMenu(std::string selected_path, bool listMode 
     {
         if (f.is_regular_file())
         {
-            files.push_back(f.path().string());
-            if (listMode)
+            std::string file_name = f.path().string();
+            if (filter_)
             {
-                std::cout << "[" << i << "] " << f.path().string() << std::endl;
+                if (checkExtension(file_name))
+                {
+                    addFile(files, file_name, i, listMode);
+                }
             }
-            i++;
+            else
+            {
+                addFile(files, file_name, i, listMode);
+            }
         }
     }
 
@@ -134,10 +194,12 @@ std::vector<std::string> select(std::vector<std::string> files)
     std::cout << "Please input the indexes of the file(s) you want to format (multiple files are separated by spaces) : ";
     while (std::cin >> file_index)
     {
+        //file not listed conditional
         if (file_index == files.size() - 1) {
             std::cout << "What file would you like to format : ";
             std::cin >> files.at(file_index);
         }
+
         inputs.push_back(files.at(file_index));
         //break the infinite cin loop once enter is pressed, failbit prevents non int inputs
         if (std::iscntrl(std::cin.get()))
